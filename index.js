f10c42f7f2845dbb9fd5c072d95c8c62293c94cb
Lactor <lactorjm4@gmail.com>
2023-07-20 18:06:30 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+const random = generateRandomNumber(1,10);
+
+let num3=22;
+let num4=6;
+const mod= num3 % num4;
+
+const max = 20;
